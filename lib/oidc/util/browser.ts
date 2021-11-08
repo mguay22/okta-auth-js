2948cf7a279e121f02d144796e8e67c4db85cabb
Michael <guay.me@gmail.com>
2021-11-08 13:07:28 -0500

    Remove origin check

--- lib/oidc/util/browser.ts
+++ lib/oidc/util/browser.ts
@@ -12,14 +12,14 @@
  */
 /* global window, document */
 /* eslint-disable complexity, max-statements */
-import { AuthSdkError } from '../../errors';
-import { OktaAuth } from '../../types';
+import { AuthSdkError } from "../../errors";
+import { OktaAuth } from "../../types";
 
 export function addListener(eventTarget, name, fn) {
   if (eventTarget.addEventListener) {
     eventTarget.addEventListener(name, fn);
   } else {
-    eventTarget.attachEvent('on' + name, fn);
+    eventTarget.attachEvent("on" + name, fn);
   }
 }
 
@@ -27,22 +27,24 @@ export function removeListener(eventTarget, name, fn) {
   if (eventTarget.removeEventListener) {
     eventTarget.removeEventListener(name, fn);
   } else {
-    eventTarget.detachEvent('on' + name, fn);
+    eventTarget.detachEvent("on" + name, fn);
   }
 }
 
 export function loadFrame(src) {
-  var iframe = document.createElement('iframe');
-  iframe.style.display = 'none';
+  var iframe = document.createElement("iframe");
+  iframe.style.display = "none";
   iframe.src = src;
 
   return document.body.appendChild(iframe);
 }
 
 export function loadPopup(src, options) {
-  var title = options.popupTitle || 'External Identity Provider User Authentication';
-  var appearance = 'toolbar=no, scrollbars=yes, resizable=yes, ' +
-    'top=100, left=500, width=600, height=600';
+  var title =
+    options.popupTitle || "External Identity Provider User Authentication";
+  var appearance =
+    "toolbar=no, scrollbars=yes, resizable=yes, " +
+    "top=100, left=500, width=600, height=600";
   return window.open(src, title, appearance);
 }
 
@@ -50,7 +52,6 @@ export function addPostMessageListener(sdk: OktaAuth, timeout, state) {
   var responseHandler;
   var timeoutId;
   var msgReceivedOrTimeout = new Promise(function (resolve, reject) {
-
     responseHandler = function responseHandler(e) {
       if (!e.data || e.data.state !== state) {
         // A message not meant for us
@@ -61,22 +62,21 @@ export function addPostMessageListener(sdk: OktaAuth, timeout, state) {
       // This may happen if apps with different issuers are running on the same host url
       // If they share the same storage key, they may read and write tokens in the same location.
       // Common when developing against http://localhost
-      if (e.origin !== sdk.getIssuerOrigin()) {
-        return reject(new AuthSdkError('The request does not match client configuration'));
-      }
+      // if (e.origin !== sdk.getIssuerOrigin()) {
+      //   return reject(new AuthSdkError('The request does not match client configuration'));
+      // }
       resolve(e.data);
     };
 
-    addListener(window, 'message', responseHandler);
+    addListener(window, "message", responseHandler);
 
     timeoutId = setTimeout(function () {
-      reject(new AuthSdkError('OAuth flow timed out'));
+      reject(new AuthSdkError("OAuth flow timed out"));
     }, timeout || 120000);
   });
 
-  return msgReceivedOrTimeout
-    .finally(function () {
-      clearTimeout(timeoutId);
-      removeListener(window, 'message', responseHandler);
-    });
+  return msgReceivedOrTimeout.finally(function () {
+    clearTimeout(timeoutId);
+    removeListener(window, "message", responseHandler);
+  });
 }
